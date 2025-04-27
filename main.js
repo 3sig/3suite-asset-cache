b38abd3f9691b47d06a85004aea1f0c09e9cd006
Aaron Santiago <me@aaron.work>
2025-04-26 23:22:49 -0400

    revert back to expressjs re-request system from proxy. added support for multipart form requests

--- main.js
+++ main.js
@@ -1,55 +1,111 @@
+
 import config from "3lib-config";
 import fs from "fs";
 import cors from "cors";
+import process from "process";
 import express from "express";
-import {
-  createProxyMiddleware,
-  responseInterceptor,
-} from "http-proxy-middleware";
+import { Readable } from "stream";
+import { finished } from "stream/promises";
+import multer from "multer";
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+// const upload = multer({ dest: 'uploads/' });
 
 config.init();
 
+let sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const app = express();
 
-let assetId = 0;
-const proxyMiddleware = createProxyMiddleware({
-  target: config.get("destinationServer"),
-  changeOrigin: true,
-  selfHandleResponse: true,
-  on: {
-    proxyRes: responseInterceptor(
-      async (responseBuffer, proxyRes, req, res) => {
-        let filename =
-          config.get("filenamePrefix", "") +
-          Date.now() +
-          "-" +
-          ("0000" + assetId++).slice(-2) +
-          config.get("fileExtension", "");
-        let filepath = config.get("directory") + "/" + filename;
-
-        if (req.headers["3suite-filepath"]) {
-          filepath = req.headers["3suite-filepath"];
-        }
-
-        try {
-          fs.writeFileSync(filepath, responseBuffer);
-        } catch (err) {
-          console.error("Error writing to file:", err);
-        }
-
-        res.setHeader("content-type", "text/plain");
-        return filename;
-      },
-    ),
-  },
-  logger: console,
-});
-app.use("/", proxyMiddleware);
+app.use(upload.any());
+app.use(express.json({ limit: "500mb" })); // for parsing application/json
+app.use(express.urlencoded({ limit: "500mb" }));
 app.use(cors());
+
+let assetId = 0;
+async function handleRequest(req, res, method) {
+  console.log(req.body);
+  try {
+    console.log("executing request", config.get("destinationServer") + req.url);
+    let modifiedHeaders = { ...req.headers };
+
+    delete modifiedHeaders["content-length"];
+
+    let params = {
+      method: method,
+      headers: modifiedHeaders,
+    };
+
+    if (method == "POST") {
+      let contentType = req.headers["content-type"];
+      if (contentType.startsWith("application/json")) {
+        params.body = JSON.stringify(req.body);
+      }
+      else if (contentType.startsWith("multipart/form-data")) {
+        delete params.headers["content-type"];
+        let formData = new FormData();
+        for (let key in req.body) {
+          formData.append(key, req.body[key]);
+        }
+        for (let file of req.files) {
+          formData.append(file.fieldname, new Blob([file.buffer], { type: file.mimetype }), file.originalname);
+        }
+        params.body = formData;
+      }
+    }
+    console.log("params", params);
+
+    let filename =
+      config.get("filenamePrefix", "") +
+      Date.now() +
+      "-" +
+      ("0000" + assetId++).slice(-2) +
+      config.get("fileExtension", "");
+    let filepath = config.get("directory") + "/" + filename;
+
+    if (req.headers["3suite-filepath"]) {
+      filepath = req.headers["3suite-filepath"];
+    }
+
+    const stream = fs.createWriteStream(filepath);
+    let response = await fetch(
+      "http" +
+        (config.get("useHttps", false) ? "s" : "") +
+        "://" +
+        config.get("destinationServer") +
+        req.url,
+      params,
+    );
+
+    await finished(Readable.fromWeb(response.body).pipe(stream));
+
+    res.send(filename);
+  } catch (e) {
+    console.log("error with request");
+    console.log(e);
+    res.end("error");
+  }
+}
+
+
+
+app.post("*", (req, res) => {
+  handleRequest(req, res, "POST");
+});
+
+app.get("*", (req, res) => {
+  handleRequest(req, res, "GET");
+});
+
+app.put("*", (req, res) => {
+  handleRequest(req, res, "PUT");
+});
+
+app.options("*", (req, res) => {
+  handleRequest(req, res, "OPTIONS");
+});
 
 let port = config.get("port");
 app.listen(port, () => {
-  console.log(
-    `3suite-asset-cache listening on port ${port}, forwarding to ${config.get("destinationServer")}`,
-  );
+  console.log(`3suite-asset-cache listening on port ${port}`);
 });
